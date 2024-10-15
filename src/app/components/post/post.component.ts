efa49cd28853080ce2b1185d067fd4eaea8e366f
Kenneth Hendrix <kennethhendrix72@gmail.com>
2024-10-14 22:30:43 -0400

    Comments and expanded posts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { Post } from '../../models/post.model';
 import { CommentsComponent } from "../comments/comments.component";
 import { TimestampToDatePipe } from '../../pipes/timestamp-to-date.pipe';
@@ -11,12 +11,28 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   @Input({ required: true }) post!: Post;
+  @Output() expandPost = new EventEmitter<void>();
 
   private toastr = inject(ToastrService);
 
-  showComments() {
-    this.toastr.info('Comments are still in development', 'Coming Soon');
+  textToShow: string = "";
+
+  ngOnInit(): void {
+    if (this.post.body.length > 700) {
+      this.textToShow = this.post.body.slice(0, 1500) + "...";
+    } else {
+      this.textToShow = this.post.body;
+    }
   }
+
+  showComments() {
+    this.expandPost.emit();
+  }
+
+  expand() {
+    this.expandPost.emit();
+  }
+
 }
