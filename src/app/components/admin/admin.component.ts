d11e8eb7ae0ccc7a6856e3d023412b3bf36155ef
Kenneth Hendrix <kennethhendrix72@gmail.com>
2024-10-14 23:23:01 -0400

    Material

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -6,6 +6,7 @@ import { NewPostComponent } from "./new-post/new-post.component";
 import { EditPostComponent } from "./edit-post/edit-post.component";
 import { DeletePostComponent } from "./delete-post/delete-post.component";
 import { ViewThreatsComponent } from "./view-threats/view-threats.component";
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-admin',
@@ -17,6 +18,7 @@ import { ViewThreatsComponent } from "./view-threats/view-threats.component";
     EditPostComponent,
     DeletePostComponent,
     ViewThreatsComponent,
+    MatTabsModule,
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
