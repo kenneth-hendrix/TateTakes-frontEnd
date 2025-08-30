4f7c2e8bf8412efbc78a1689fd6a7fe7445ccdc5
Jack Hendrix <jack@khendrix.net>
2025-08-30 16:08:08 -0400

    Draft save duplication (#31)
    * Fixed draft dupe issue
    
    * upgrade dependencies
    
    * Update actions

--- src/app/components/admin/new-post/new-post.component.ts
+++ src/app/components/admin/new-post/new-post.component.ts
@@ -28,7 +28,7 @@ export class NewPostComponent implements OnInit {
 
   postForm: FormGroup;
   draftId = '';
-  autosaveText = 'Post not saved!';
+  autosaveText = 'Draft not saved!';
   autosaving = false;
   selectedFile: File | null = null;
   imageUrl = '';
@@ -102,7 +102,7 @@ export class NewPostComponent implements OnInit {
           this.postForm.reset();
           this.imageUrl = '';
           this.draftsService.deleteDraft(this.draftId).then(() => {
-            this.autosaveText = 'Post not saved!';
+            this.autosaveText = 'Draft not saved!';
           });
           this.toastr.success(
             `Your post, ${title}, has been published successfully`,
@@ -125,21 +125,44 @@ export class NewPostComponent implements OnInit {
     this.spinner.show();
     const { title, body } = this.postForm.value;
     const formattedText = body.replace(/\n/g, '<br>');
-    this.draftsService
-      .newDraft(title, formattedText, this.imageUrl)
-      .then(() => {
-        this.postForm.reset();
-        this.imageUrl = '';
-        this.toastr.success(`Your draft was saved successfully`, 'Success');
-        this.draftsService.draftCreated();
-      })
-      .catch((error) => {
-        console.error(error);
-        this.toastr.error('Please try again later', 'Something went wrong');
-      })
-      .finally(() => {
-        this.spinner.hide();
-      });
+    const post = {
+      title: title,
+      image: this.imageUrl,
+      body: formattedText,
+      date: new Date(),
+    };
+    if (this.draftId) {
+      this.draftsService
+        .updateDraft(this.draftId, post)
+        .then(() => {
+          this.postForm.reset();
+          this.imageUrl = '';
+          this.toastr.success(`Your draft was saved successfully`, 'Success');
+        })
+        .catch((error) => {
+          console.error(error);
+          this.toastr.error('Please try again later', 'Something went wrong');
+        })
+        .finally(() => {
+          this.spinner.hide();
+        });
+    } else {
+      this.draftsService
+        .newDraft(title, formattedText, this.imageUrl)
+        .then(() => {
+          this.postForm.reset();
+          this.imageUrl = '';
+          this.toastr.success(`Your draft was saved successfully`, 'Success');
+          this.draftsService.draftCreated();
+        })
+        .catch((error) => {
+          console.error(error);
+          this.toastr.error('Please try again later', 'Something went wrong');
+        })
+        .finally(() => {
+          this.spinner.hide();
+        });
+    }
   }
 
   get isValidDraft(): boolean {
